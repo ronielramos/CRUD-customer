1ee3740b5c8188a6ce2d3a593b0c1e5e5c88dfc8
Roniel <roniel_ramos@hotmail.com>
2020-10-04 17:42:42 -0300

    feat: criar setup inicial do server

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,42 @@
+import 'express-async-errors'
+import 'reflect-metadata'
+
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+
+import BadRequestError from '../../errors/BadRequestError'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import neo4jDriver from '../neo4j/index'
+import { container } from 'tsyringe'
+import ILoggerProvider from '../../container/providers/LoggerProvider/ILoggerProvider'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const logger = container.resolve<ILoggerProvider>('LoggerProvider')
+
+// app.use(routes)
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof BadRequestError) {
+    return res
+      .status(err.statusCode)
+      .json({ message: err.message, status: 'error' })
+  }
+
+  logger.error(err)
+
+  return res
+    .status(500)
+    .json({ message: 'Internal server error', status: 'error' })
+})
+
+app.listen(process.env.PORT, async () => {
+  await neo4jDriver.verifyConnectivity()
+
+  logger.log('✅ - Server on', process.env.PORT)
+})
